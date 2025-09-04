70a48bdfec66aba60f45ccd7a694069eee210443
Marcos Nitola <marcos123.mdnr@gmail.com>
2025-09-04 13:11:09 -0500

    init: generate Spring Boot project structure

---
+++ src/main/java/com/uistify/backend/UistifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uistify.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UistifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UistifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uistify/backend/UistifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uistify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UistifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
