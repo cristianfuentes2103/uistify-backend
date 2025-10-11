a981e46178aa9688bafbef2303b10bcab8a156a9
Marcos Nitola <marcos123.mdnr@gmail.com>
2025-10-11 18:40:25 -0500

    Fix: lombok annotations added

--- src/main/java/com/uistify/backend/persistence/model/PlaylistSong.java
+++ src/main/java/com/uistify/backend/persistence/model/PlaylistSong.java
@@ -1,7 +1,15 @@
 package com.uistify.backend.persistence.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "playlist_song")
 public class PlaylistSong {
