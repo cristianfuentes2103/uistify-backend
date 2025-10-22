62bc46cf85f53e1f3cf7d9fe6c584a938e88b2f7
cristian <cristiandavidfuentes@hotmail.com>
2025-10-22 11:10:53 -0500

    CRF-Feat: fix bean to upload File

--- src/main/java/com/uistify/backend/service/impl/FileServiceImpl.java
+++ src/main/java/com/uistify/backend/service/impl/FileServiceImpl.java
@@ -3,6 +3,7 @@ package com.uistify.backend.service.impl;
 import com.uistify.backend.presentation.dto.ErrorDto;
 import com.uistify.backend.service.IFileService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,17 +11,10 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-
-import java.net.URI;
 
 @Service
 @Slf4j
@@ -29,36 +23,15 @@ public class FileServiceImpl implements IFileService {
     @Value("${bucket.name}")
     String bucketName;
 
-    @Value("${access.key}")
-    String accessKey;
-
-    @Value("${secret.key}")
-    String secretKey;
-
-    @Value("${region.key}")
-    String region;
-
-    @Value("${url.minio}")
-    String url;
+    @Autowired
+    S3Client s3Client;
 
     @Override
     public ResponseEntity<Object> getFile(String objectKey) {
         try {
-
-            AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
-            S3Client s3Client = S3Client
-                    .builder()
-                    .region(Region.of(region))
-                    .endpointOverride(URI.create(url))
-                    .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
-                    .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                    .build();
-
-            log.info("upload file to {}", url);
-
             ResponseInputStream<GetObjectResponse> responseInputStream = s3Client.getObject(GetObjectRequest.builder()
-                    .bucket(bucketName).key(objectKey)
+                    .bucket(bucketName)
+                    .key(objectKey)
                     .build()
             );
             MultiValueMap<String, String> headers = new HttpHeaders();
