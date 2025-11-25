4709582dec0f714188ea2047235685e9d22d70a0
Cristian Fuentes <cdfuente@nequi.com>
2025-11-24 20:07:37 -0500

    CRF-Feat: fix type of field

--- src/main/java/com/uistify/backend/application/service/PlaylistService.java
+++ src/main/java/com/uistify/backend/application/service/PlaylistService.java
@@ -39,7 +39,7 @@ public class PlaylistService implements PlaylistUseCase {
 	public Playlist getPublicPlaylist(Long playlistId){
         Playlist publicPlaylist = playlistRepository.findById(playlistId)
                 .orElseThrow(() -> new IllegalArgumentException("Playlist not found"));
-		if (!publicPlaylist.isPublicPlaylist()){
+		if (!publicPlaylist.getPublicPlaylist()){
 			throw new SecurityException("Playlist is not public");
 		}
 		return publicPlaylist;
