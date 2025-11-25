d9e8e762e3d808a5d5df6fd84a31ab7fb02b38f5
Marcos Nitola <marcos123.mdnr@gmail.com>
2025-11-24 21:30:28 -0500

    fix: fix updatePlaylist method

--- src/main/java/com/uistify/backend/application/service/PlaylistService.java
+++ src/main/java/com/uistify/backend/application/service/PlaylistService.java
@@ -68,6 +68,7 @@ public class PlaylistService implements PlaylistUseCase {
         Playlist toPersist = existing.toBuilder()
                 .title(playlist.getTitle())
                 .description(playlist.getDescription())
+				.publicPlaylist(playlist.isPublicPlaylist())
                 .build();
         return playlistRepository.save(toPersist);
     }
